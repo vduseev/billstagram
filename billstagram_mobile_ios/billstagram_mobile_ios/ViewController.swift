4f2bdf99873fa613bebfda107bc78d5a215825c7
vduseev <vagiz.d@gmail.com>
2016-11-28 17:27:35 +0100

    Pity attempts to run my first iOS app

--- billstagram_mobile_ios/billstagram_mobile_ios/ViewController.swift
+++ billstagram_mobile_ios/billstagram_mobile_ios/ViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var pickedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func cameraButtonAction(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        }
+    }
+    
+    @IBAction func photoLibraryAction(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        let imageData = UIImageJPEGRepresentation(pickedImageView.image!, 0.6)
+        let compressedJPEGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pickedImageView.contentMode = .scaleAspectFit
+            pickedImageView.image = pickedImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func saveNotice() {
+    
+    }
 }
-
